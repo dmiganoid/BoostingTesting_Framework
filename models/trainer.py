132831fe238e6ec481d2d4de1b2a6c6095b42c3e
Georgy Mochalov <89660772+Need2BuySSD@users.noreply.github.com>
2025-03-15 17:09:39 +0300

    output improvements and fixes

--- models/trainer.py
+++ models/trainer.py
@@ -10,6 +10,7 @@ from functools import wraps
 # Ща захардкодил, завтра исправлю. Тут беда с импортом из родительской директории
 from os import getpid
 from psutil import Process, cpu_count
+import pandas as pd
 
 def get_memory_usage_mb():
     process = Process(getpid())
@@ -23,7 +24,7 @@ def timed_wrapper(func, *args, **kwargs): # picklable workaround
     end_time = time.time()
     return result, end_time-start_time
 
-def train_test_model(algorithm_class, params, X_train, X_test, y_train, y_test, results_path):
+def train_test_model(algorithm_class, params, X_train, X_test, y_train, y_test, results_path, ind=""):
     model = algorithm_class(**params)
     
     start_time = time.time()
@@ -35,15 +36,14 @@ def train_test_model(algorithm_class, params, X_train, X_test, y_train, y_test, 
     model.predict(X_test)
     inference_time = time.time() - start_time
 
-    np.savetxt(f'{results_path}/train_{algorithm_class.__name__}.csv', model.predict(X_train), delimiter=",")
-    np.savetxt(f'{results_path}/test_{algorithm_class.__name__}.csv', model.predict(X_test), delimiter=",")
-    np.savetxt(f'{results_path}/train-dataset.csv', np.hstack((X_train, y_train.reshape(X_train.shape[0], 1))), delimiter=",")
-    np.savetxt(f'{results_path}/test-dataset.csv', np.hstack((X_test, y_test.reshape(X_test.shape[0], 1))), delimiter=",")
+    np.savetxt(f'{results_path}/pred_train_{algorithm_class.__name__}{ind}.csv', model.predict(X_train), delimiter=",")
+    np.savetxt(f'{results_path}/pred_test_{algorithm_class.__name__}{ind}.csv', model.predict(X_test), delimiter=",")
  
     output_params = params
     if 'estimator' in output_params.keys():
         output_params['estimator'] = str(output_params['estimator'])
     results = {
+                    "algorithm" : algorithm_class.__name__,
                     "model_params" : output_params,
                     "train_time_sec": train_time,
                     "inference_time_sec": inference_time,
@@ -142,22 +142,23 @@ class BoostingBenchmarkTrainer:
         self.algorithms = algorithms
         
     def fit_and_evaluate(self, X, y, random_state=42, test_size=0.15, results_path="results", test_name="test"):
-        results = {}
+        results = []
         X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=random_state, test_size=test_size)
         pool = Pool(processes=cpu_count(logical=False))
         print(f"Starting {test_name}")
         mkdir(f'{results_path}/{test_name}')
+        np.savetxt(f'{results_path}/{test_name}/train-dataset.csv', np.hstack((X_train, y_train.reshape(X_train.shape[0], 1))), delimiter=",")
+        np.savetxt(f'{results_path}/{test_name}/test-dataset.csv', np.hstack((X_test, y_test.reshape(X_test.shape[0], 1))), delimiter=",")
         for algorithm_class, algorithm_param_grid in self.algorithms:
             if algorithm_class == None:
                 continue
             threads = []
             print(f"Training {algorithm_class.__name__}")
-            for params in ParameterGrid(algorithm_param_grid):
-                threads.append(pool.apply_async(train_test_model, args=[algorithm_class, params, X_train, X_test, y_train, y_test, f'{results_path}/{test_name}']))
-            for model_i in range(len(threads)):
-                results[f"{algorithm_class.__name__}_{model_i}"] = threads[model_i].get(timeout=None)
+            for ind, params in enumerate(ParameterGrid(algorithm_param_grid)):
+                threads.append(pool.apply_async(train_test_model, args=[algorithm_class, params, X_train, X_test, y_train, y_test, f'{results_path}/{test_name}'], kwds={"ind" : ind}))
+            for thread in threads:
+                results.append(thread.get(timeout=None))
         pool.close()
-        with open(f'{results_path}/{test_name}/results.json', 'w') as file:
-            json.dump(results, file)
+        pd.DataFrame(results).to_csv(f'{results_path}/{test_name}/results.json', sep=",")
         print(f'Finished {test_name}')
         return 0
